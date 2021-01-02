18fac8d5453f43e66836bf119de21dea155bcc55
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-01-02 20:32:22 -0300

    Added main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#define PRINT(txt) std::cout << txt;
+#define PRINTLN(txt) std::cout << txt << std::endl;
+
+int main(int argc, char* argv[]) {
+	PRINTLN("---- AvoidGator ----")
+	PRINTLN("Try to don't become into an alligator!")
+	return 0;
+}
