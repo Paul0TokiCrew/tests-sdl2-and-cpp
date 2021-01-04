e75d39c30a8d92e272e656292e3a8748c781fde0
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-01-04 13:28:40 -0300

    Added player movement

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,9 @@
 #define PRINT(txt) std::cout << txt;
 #define PRINTLN(txt) std::cout << txt << std::endl;
 
-#define LEFT 0
-#define RIGHT 1
+#define IDLE 0
+#define LEFT 1
+#define RIGHT 2
 
 
 
@@ -22,10 +23,11 @@ int main(int argc, char* argv[]) {
 	PRINTLN("Try to don't become into an alligator!")
 
 	bool game_over = false;
+	int x = 0, y = 0, w = 64, h = 64;
 	window win = window("AvoidGator", W, H);
-	player_data dir = player_data(LEFT, RIGHT);
+	player_data dir = player_data(IDLE, RIGHT);
 
-	sprite ademir = sprite(win, "res/sprites/ademir/Ademir Jr..png", { 0, 0, 20, 20 }, { 0, 0, 64, 64 }, 4, 1);
+	sprite ademir = sprite(win, "res/sprites/ademir/Ademir Jr..png", { 0, 0, 20, 20 }, { x, y, w, h }, 4, 1);
 
 	auto update_datas = [&] () -> void {
 		const Uint8* key = SDL_GetKeyboardState(nullptr);
@@ -35,6 +37,18 @@ int main(int argc, char* argv[]) {
 
 		else if (key[SDL_SCANCODE_RIGHT])
 			dir.change_current_data(RIGHT);
+
+		else
+			dir.change_current_data(IDLE);
+
+	};
+
+	auto update_pos = [&] () -> void {
+		if (dir.get_current_data() == LEFT)
+			x -= 5;
+
+		else if (dir.get_current_data() == RIGHT)
+			x += 5;
 
 	};
 
@@ -47,9 +61,10 @@ int main(int argc, char* argv[]) {
 				game_over = true;
 
 		update_datas();
-		PRINTLN(dir.get_current_data())
+		update_pos();
 
 		win.clear();
+		ademir.change_pos(x, y);
 		ademir.draw();
 		ademir.advance_x_frame();
 		win.update();
