12125110afd1ff53b5895746ca8318f0431a0338
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-11 15:56:44 -0300

    spongebob

--- rpg/src/main.cpp
+++ rpg/src/main.cpp
@@ -5,13 +5,25 @@
 #include <window.hpp>
 #include <image.hpp>
 
+
+
+#define PRINTLN(txt) std::cout << txt << std::endl;
+
+
+
 int main() {
 	window win = window("RPG", 720, 480);
+
+	vec2f bob = vec2f(0, 0);
+	vec2f bob_view = vec2f(bob.x + 1, bob.y + 1);
+
 	bool running = true;
 
 	int i = 0;
 
 	SDL_Event evn;
+
+	PRINTLN((bob - bob_view).normalize());
 
 	while (running) {
 
