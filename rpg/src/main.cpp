55129ab301537c5138334d81a657311550acc1db
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-12 19:58:21 -0300

    bob is yes chad
    Yes.

--- rpg/src/main.cpp
+++ rpg/src/main.cpp
@@ -11,13 +11,14 @@
 
 
 
-void update_pos(vec2f& ent, vec2f& ent_vel, const float delta_time, const vec2f max_vel);
+void update_pos(vec2f& ent, image& ent_img, vec2f& ent_vel, const vec2f max_vel, const float delta_time);
 
 int main() {
 	window win = window("RPG", 720, 480);
 
 	vec2f bob = vec2f(0, 0);
 	vec2f bob_vel = vec2f(1, 1);
+	image bob_img = image(win, "res/chad.png", { 0, 0, 600, 600 }, { 0, 0, 200, 200 });
 
 	bool running = true;
 	int i = 0;
@@ -33,13 +34,14 @@ int main() {
 				running = false;
 
 
-		update_pos(bob, bob_vel, 0.01f, vec2f(3, 3));
+		update_pos(bob, bob_img, bob_vel, vec2f(3, 3), 0.01f);
 
 		PRINTLN("bob pos: " << bob)
 		PRINTLN("bob vel: " << bob_vel)
 		PRINTLN("---------------------------")
 
 		win.clear(i, i, i);
+		bob_img.draw();
 		win.update();
 
 		if (i < 255)
@@ -49,10 +51,11 @@ int main() {
 			i = 0;
 
 	}
+
 	return 0;
 }
 
-void update_pos(vec2f& ent, vec2f& ent_vel, const float delta_time, const vec2f max_vel) {
+void update_pos(vec2f& ent, image& ent_img, vec2f& ent_vel, const vec2f max_vel, const float delta_time) {
 	ent.x += ent_vel.x * delta_time,
 	ent.y += ent_vel.y * delta_time;
 
@@ -71,5 +74,7 @@ void update_pos(vec2f& ent, vec2f& ent_vel, const float delta_time, const vec2f 
 
 	else if (std::ceil(ent_vel.y) > std::ceil(max_vel.y))
 		ent_vel.y -= delta_time;
+
+	ent_img.change_pos(ent.x, ent.y);
 
 }
