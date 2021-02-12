efaefd1a43db864979805bb25b772a14246a9e35
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-12 15:00:39 -0300

    Removed "bob_view" object
    now he is blind (dont laugh)

--- rpg/src/main.cpp
+++ rpg/src/main.cpp
@@ -11,13 +11,10 @@
 
 
 
-void update_view(vec2f& ent_view);
-
 int main() {
 	window win = window("RPG", 720, 480);
 
 	vec2f bob = vec2f(0, 0);
-	vec2f bob_view = vec2f(bob.x + 1, bob.y + 1);
 
 	bool running = true;
 	int i = 0;
@@ -26,36 +23,9 @@ int main() {
 
 	while (running) {
 
-		update_view(bob_view);
-
-		vec2f bob_view_dir = vec2f((bob_view - bob));
-
 		while (SDL_PollEvent(&evn))
 			if (evn.type == SDL_QUIT)
 				running = false;
-
-
-		PRINTLN("------------------")
-
-		if (bob_view_dir.y > 0)
-			PRINTLN("bob is looking down")
-
-		else if (bob_view_dir.y == 0)
-			PRINTLN("bob is looking middle")
-
-		else
-			PRINTLN("bob is looking up")
-
-
-
-		if (bob_view_dir.x > 0)
-			PRINTLN("bob is looking right")
-
-		else if (bob_view_dir.x == 0)
-			PRINTLN("bob is looking middle")
-
-		else
-			PRINTLN("bob is looking left")
 
 		win.clear(i, i, i);
 		win.update();
@@ -68,21 +38,4 @@ int main() {
 
 	}
 	return 0;
-}
-
-void update_view(vec2f& ent_view) {
-	const Uint8* key = SDL_GetKeyboardState(nullptr);
-
-	if (key[SDL_SCANCODE_UP])
-		ent_view.y -= 0.1f;
-
-	if (key[SDL_SCANCODE_DOWN])
-		ent_view.y += 0.1f;
-
-	if (key[SDL_SCANCODE_RIGHT])
-		ent_view.x += 0.1f;
-
-	if (key[SDL_SCANCODE_LEFT])
-		ent_view.x -= 0.1f;
-
 }
