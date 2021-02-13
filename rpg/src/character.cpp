4d742cd18682f68d2ff4e3f5036783feb0eacddc
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-13 14:08:48 -0300

    eae 2

--- rpg/src/character.cpp
+++ rpg/src/character.cpp
@@ -26,15 +26,23 @@ void character::update_pos(const float delta_time) {
 
 void character::update_datas(const Uint8* key) {
 	if (key[SDL_SCANCODE_UP])
-		max_vel.y = -1;
+		max_vel.y = -15;
 
 	if (key[SDL_SCANCODE_DOWN])
-		max_vel.y = 1;
+		max_vel.y = 15;
+
+	if (!(key[SDL_SCANCODE_UP] || key[SDL_SCANCODE_DOWN]))
+		max_vel.y = 0;
+
+
 
 	if (key[SDL_SCANCODE_RIGHT])
-		max_vel.x = 1;
+		max_vel.x = 15;
 
 	if (key[SDL_SCANCODE_LEFT])
-		max_vel.x = -1;
+		max_vel.x = -15;
+
+	if (!(key[SDL_SCANCODE_RIGHT] || key[SDL_SCANCODE_LEFT]))
+		max_vel.x = 0;
 
 }
