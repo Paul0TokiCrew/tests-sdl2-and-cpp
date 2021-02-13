2ba99275ea6dce081edb944d46a5cc60d8fc2e9a
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-13 12:45:23 -0300

    Added getters and setters for "character" class attributes

--- rpg/include/character.hpp
+++ rpg/include/character.hpp
@@ -15,6 +15,10 @@ public:
 	pos(pos), vel(vel), max_vel(max_vel) { }
 	~character() { }
 
+	vec2f get_pos() const { return this->pos; }
+	vec2f get_vel() const { return this->vel; }
+	vec2f get_max_vel() const { return this->max_vel; }
+
 	void update_pos(const float delta_time);
 
 };
