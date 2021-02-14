e9d32869919f4db2cc7421ea00fa7a3c9aba4708
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-14 10:18:22 -0300

    Added "character::goal_vel" attribute

--- rpg/include/character.hpp
+++ rpg/include/character.hpp
@@ -9,15 +9,16 @@
 
 class character {
 private:
-	vec2f pos, vel, max_vel;
+	vec2f pos, vel, goal_vel, max_vel;
 
 public:
 	character(vec2f pos, vec2f vel, vec2f max_vel) :
-	pos(pos), vel(vel), max_vel(max_vel) { }
+	pos(pos), vel(vel), goal_vel(vel) max_vel(max_vel) { }
 	~character() { }
 
 	vec2f get_pos() const { return this->pos; }
 	vec2f get_vel() const { return this->vel; }
+	vec2f get_goal_vel() const { return this->goal_vel; }
 	vec2f get_max_vel() const { return this->max_vel; }
 
 	void update_pos(const float delta_time, const float atr);
