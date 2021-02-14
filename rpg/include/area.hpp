cb7e0e5d6b4bceca79857b5e5fb7e8d5ff49334a
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-14 17:15:31 -0300

    Added "area_manager::obj" attribute

--- rpg/include/area.hpp
+++ rpg/include/area.hpp
@@ -2,11 +2,16 @@
 
 
 
+#include <vector>
+#include <utility>
 #include <vec2f.hpp>
 
 
 
 class area_manager {
+private:
+    std::vector<std::pair<vec2f, vec2f>> objs;
+
 public:
     area_manager() { }
     ~area_manager() { }
