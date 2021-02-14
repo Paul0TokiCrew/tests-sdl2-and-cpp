14b7de951a2707f7740755e3c50180317fab7fd9
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-14 17:20:45 -0300

    Added "character::register_area(const vec2f, const vec2f)" method

--- rpg/include/area.hpp
+++ rpg/include/area.hpp
@@ -10,10 +10,12 @@
 
 class area_manager {
 private:
-    std::vector<std::pair<vec2f, vec2f>> objs;
+	std::vector<std::pair<vec2f, vec2f>> area_pos;
 
 public:
-    area_manager() { }
-    ~area_manager() { }
+	area_manager() { }
+	~area_manager() { }
+
+	void register_area(const vec2f xy, const vec2f wh);
 
 };
--- rpg/src/area.cpp
+++ rpg/src/area.cpp
@@ -1,1 +1,7 @@
 #include <area.hpp>
+
+
+
+void area_manager::register_area(const vec2f xy, const vec2f wh) {
+	this->area_pos.push_back(std::make_pair(xy, wh));
+}
