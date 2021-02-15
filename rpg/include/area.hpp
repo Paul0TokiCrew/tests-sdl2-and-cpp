d99a0fe9ca5efca8935916f4ae41c1cc5435f933
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-15 13:25:01 -0300

    idk

--- rpg/include/area.hpp
+++ rpg/include/area.hpp
@@ -10,12 +10,16 @@
 
 class area_manager {
 private:
-	std::vector<std::pair<vec2f, vec2f>> rec_areas;
+	std::vector<std::pair<vec2f, vec2f>> line_areas;
+	std::vector<std::pair<vec2f, float>> ang_areas;
 
 public:
 	area_manager() { }
 	~area_manager() { }
 
-	void register_rec_area(const vec2f xy, const vec2f wh);
+	void register_line(const vec2f xy1, const vec2f xy2);
+	void register_rec(const vec2f xy1, const vec2f xy2);
+	void register_ang(const vec2f xy, const float r, const float ang);
+	void register_circle(const vec2f xy, const int r);
 
 };
--- rpg/src/area.cpp
+++ rpg/src/area.cpp
@@ -2,6 +2,3 @@
 
 
 
-void area_manager::register_rec_area(const vec2f xy, const vec2f wh) {
-	this->rec_areas.push_back(std::make_pair(xy, wh));
-}
