f2c5958a5cab84d50fd24682f3495bcb841ca287
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-15 17:56:29 -0300

    a mistake that i often make is to try too complex things for me

--- rpg/include/area.hpp
+++ rpg/include/area.hpp
@@ -11,18 +11,14 @@
 
 class area_manager {
 private:
-	std::vector<std::pair<vec2f, vec2f>> line_areas;
-	std::vector<std::tuple<vec2f, float, float>> ang_areas;
+	std::vector<std::pair<vec2f, vec2f>> areas;
 
 public:
 	area_manager() { }
 	~area_manager() { }
 
-	void register_line(const vec2f xy1, const vec2f xy2);
 	void register_rec(const vec2f xy1, const vec2f xy2);
-	void register_ang(const vec2f xy, const float r, const float a);
-	void register_circle(const vec2f xy, const int r);
 
-	bool check_collision(const vec2f other_xy, const vec2f other_wh) const;
+	bool check_collision(const vec2f other_xy1, const vec2f other_xy2) const;
 
 };
--- rpg/src/area.cpp
+++ rpg/src/area.cpp
@@ -2,41 +2,21 @@
 
 
 
-void register_line(const vec2f xy1, const vec2f xy2) {
-	this->line_areas.push_back(std::make_pair(xy1, xy2));
-}
-
 void register_rec(const vec2f xy1, const vec2f xy2) {
-	vec2f up1 = vec2f(xy1), up2 = vec2f(xy2.x, xy1.y),
-	down1 = vec2f(xy1.x, xy2.y), down2 = vec2f(xy2),
-	right1 = vec2f(xy1), right2 = vec2f(xy1.x, xy2.y),
-	left1 = vec2f(xy2.x xy1.y), left2 = vec2f(xy2);
-
-	this->line_areas.push_back(std::make_pair(up1, up2));
-	this->line_areas.push_back(std::make_pair(down1, down2));
-	this->line_areas.push_back(std::make_pair(right1, right2));
-	this->line_areas.push_back(std::make_pair(left1, left2));
+	this->areas.push_back(std::make_pair(xy1, xy2);
 }
 
-void register_ang(const vec2f xy, const float r, const float a) {
-	if (a > 360 || a < 0)
-		return;
+bool check_collision(const vec2f other_xy1, const vec2f other_xy2) const {
+	auto i = this->areas.begin();
 
-	this->ang_areas.push_back(std::make_tuple(xy, r, a));
-}
+	for (; i != this->areas.end(); ++i) {
 
-void register_circle(const vec2f xy, const float r) {
-	this->ang_areas.push_back(std::make_tuple(xy, r, 360));
-}
-
-bool check_collision(const vec2f other_xy, const vec2f other_wh) const {
-	auto i = this->line_areas.begin();
-
-	for (; i != this->line_areas.end(); ++i) {
-
-		float line_length = vec2f(i.second - i.first).get_length();
-
-		if (a.x == b.x && f(a.x) == f(b.x))
+		if (
+			other_xy1.x <= i->second.x &&
+			other_xy2.x >= i->first.x &&
+			other_xy1.y >= i->second.y &&
+			other_xy2.y <= i->first.y
+		)
 			return true;
 
 	}
