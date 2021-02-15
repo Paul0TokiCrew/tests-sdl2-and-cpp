00843db8dbbad5a87b8a8eba736a4a6bf5d3e754
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-15 13:15:28 -0300

    Renamed "area_manager::register_area(const vec2f, const vec2f)" method
    Renamed "area_manager::areas" attribute to "area_manager::rec_areas"

--- rpg/include/area.hpp
+++ rpg/include/area.hpp
@@ -10,12 +10,12 @@
 
 class area_manager {
 private:
-	std::vector<std::pair<vec2f, vec2f>> areas;
+	std::vector<std::pair<vec2f, vec2f>> rec_areas;
 
 public:
 	area_manager() { }
 	~area_manager() { }
 
-	void register_area(const vec2f xy, const vec2f wh);
+	void register_rec_area(const vec2f xy, const vec2f wh);
 
 };
--- rpg/src/area.cpp
+++ rpg/src/area.cpp
@@ -2,6 +2,6 @@
 
 
 
-void area_manager::register_area(const vec2f xy, const vec2f wh) {
-	this->areas.push_back(std::make_pair(xy, wh));
+void area_manager::register_rec_area(const vec2f xy, const vec2f wh) {
+	this->rec_areas.push_back(std::make_pair(xy, wh));
 }
