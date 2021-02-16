8157d0623b7c6e594073ae8b8339a69de00ba32c
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-16 10:11:28 -0300

    i know ill have to fix bugs

--- rpg/src/area.cpp
+++ rpg/src/area.cpp
@@ -22,3 +22,75 @@ bool area_manager::check_collision(const vec2f other_xy1, const vec2f other_xy2)
 
 	return false;
 }
+
+bool area_manager::check_up_collision(const vec2f other_xy1, const vec2f other_xy2, const vec2f other_vel) const {
+
+	for (auto i : this->areas) {
+
+		if (
+			other_xy1.x <= i.second.x &&
+			other_xy2.x >= i.first.x &&
+			other_xy1.y <= i.second.y &&
+			other_xy2.y >= i.first.y &&
+			other_vel.y < 0
+		)
+			return true;
+
+	}
+
+	return false;
+}
+
+bool area_manager::check_down_collision(const vec2f other_xy1, const vec2f other_xy2, const vec2f other_vel) const {
+
+	for (auto i : this->areas) {
+
+		if (
+			other_xy1.x <= i.second.x &&
+			other_xy2.x >= i.first.x &&
+			other_xy1.y <= i.second.y &&
+			other_xy2.y >= i.first.y &&
+			other_vel.y > 0
+		)
+			return true;
+
+	}
+
+	return false;
+}
+
+bool area_manager::check_right_collision(const vec2f other_xy1, const vec2f other_xy2, const vec2f other_vel) const {
+
+	for (auto i : this->areas) {
+
+		if (
+			other_xy1.x <= i.second.x &&
+			other_xy2.x >= i.first.x &&
+			other_xy1.y <= i.second.y &&
+			other_xy2.y >= i.first.y &&
+			other_vel.x > 0
+		)
+			return true;
+
+	}
+
+	return false;
+}
+
+bool area_manager::check_left_collision(const vec2f other_xy1, const vec2f other_xy2, const vec2f other_vel) const {
+
+	for (auto i : this->areas) {
+
+		if (
+			other_xy1.x <= i.second.x &&
+			other_xy2.x >= i.first.x &&
+			other_xy1.y <= i.second.y &&
+			other_xy2.y >= i.first.y &&
+			other_vel.x < 0
+		)
+			return true;
+
+	}
+
+	return false;
+}
