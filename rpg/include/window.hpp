aa31489c2e1f8448f380820ebdd4451b35e667ce
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-17 13:01:38 -0300

    i forgot the ctrl+s

--- rpg/include/window.hpp
+++ rpg/include/window.hpp
@@ -17,11 +17,17 @@ public:
 	win(nullptr), ren(nullptr), w(w), h(h), flags(flags) {
 		this->win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, this->w, this->h, this->flags);
 		this->ren = SDL_CreateRenderer(this->win, -1, SDL_RENDERER_ACCELERATED);
-
 	}
 	window(const char* title, const window& wnd) :
-	win(nullptr), ren(nullptr), w(wnd.get_w()), h(wnd.get_h()), flags(wnd.get_flags()) { }
-	window(const char* title, const window& wnd) :
+	win(nullptr), ren(nullptr), w(wnd.get_w()), h(wnd.get_h()), flags(wnd.get_flags()) {
+		this->win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, this->w, this->h, this->flags);
+		this->ren = SDL_CreateRenderer(this->win, -1, SDL_RENDERER_ACCELERATED);
+	}
+	window(const char* title, const window&& wnd) :
+	win(nullptr), ren(nullptr), w(wnd.get_w()), h(wnd.get_h()), flags(wnd.get_flags()) {
+		this->win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, this->w, this->h, this->flags);
+		this->ren = SDL_CreateRenderer(this->win, -1, SDL_RENDERER_ACCELERATED);
+	}
 	~window() {
 		SDL_DestroyWindow(this->win);
 		SDL_DestroyRenderer(this->ren);
