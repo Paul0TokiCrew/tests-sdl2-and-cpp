41e5ba8c24fc79e1939602c55209b24b33ff8286
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-17 12:56:14 -0300

    Added "window::flags" attribute

--- rpg/include/window.hpp
+++ rpg/include/window.hpp
@@ -10,18 +10,12 @@ class window {
 private:
 	SDL_Window* win;
 	SDL_Renderer* ren;
-	const int w, h;
+	const int w, h, flags;
 
 public:
-	window(const char* title, const int w, const int h) :
-	win(nullptr), ren(nullptr), w(w), h(h) {
-		this->win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, SDL_WINDOW_SHOWN);
-		this->ren = SDL_CreateRenderer(this->win, -1, SDL_RENDERER_ACCELERATED);
-
-	}
-	window(const char* title, const int w, const int h, const int flags) :
-	win(nullptr), ren(nullptr), w(w), h(h) {
-		this->win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, flags);
+	window(const char* title, const int w, const int h, const int flags = SDL_WINDOW_SHOWN) :
+	win(nullptr), ren(nullptr), w(w), h(h), flags(flags) {
+		this->win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, this->w, this->h, this->flags);
 		this->ren = SDL_CreateRenderer(this->win, -1, SDL_RENDERER_ACCELERATED);
 
 	}
@@ -34,6 +28,7 @@ public:
 	SDL_Renderer* get_ren() const { return this->ren; }
 	int get_w() const { return this->w; }
 	int get_h() const { return this->h; }
+	int get_flags() const { return this->flags; }
 
 	void clear() const;
 	void clear(const int r, const int g, const int b) const;
