97e88d39e82b75de91ad3fe0faf78aaa4ff12890
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-18 18:45:33 -0300

    idk wtf im doing lmao

--- rpg/src/camera.cpp
+++ rpg/src/camera.cpp
@@ -1,1 +1,13 @@
 #include <camera.hpp>
+
+
+
+void camera::update_cam(const vec2f& new_xy1, const vec2f& new_xy2) {
+	if (
+		new_xy1 - this->ref_xy1 != this->start_xy1 ||
+		new_xy2 - this->ref_xy2 != this->start_xy2
+	) {
+		cam_pos = new_xy1 - this->ref_xy1;
+
+	}
+}
