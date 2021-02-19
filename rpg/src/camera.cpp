48eef062039bbb07e993f3bb2df995d699ea189b
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-19 14:22:30 -0300

    Updated "camera::update_cam()" method

--- rpg/src/camera.cpp
+++ rpg/src/camera.cpp
@@ -14,17 +14,17 @@ void camera::update_cam(const vec2f& new_xy1, const vec2f& new_xy2) {
 	) {
 		this->cam_pos = this->cam_pos + (new_xy1 - this->ref_xy1);
 
-		if (this->cam_pos.x > this->lim_xy1.x)
+		if (this->cam_pos.x < this->lim_xy1.x)
 			this->cam_pos.x = this->lim_xy1.x;
 
-		if (this->cam_pos.y > this->lim_xy1.y)
+		if (this->cam_pos.y < this->lim_xy1.y)
 			this->cam_pos.y = this->lim_xy1.y;
 
 		if (this->cam_pos.x + this->win.get_w() > this->lim_xy2.x)
-			this->cam_pos.x = this->lim_xy2.x;
+			this->cam_pos.x = this->lim_xy2.x - this->win.get_w();
 
 		if (this->cam_pos.y + this->win.get_h() > this->lim_xy2.y)
-			this->cam_pos.y = this->lim_xy2.y; 
+			this->cam_pos.y = this->lim_xy2.y - this->win.get_h(); 
 
 	}
 
