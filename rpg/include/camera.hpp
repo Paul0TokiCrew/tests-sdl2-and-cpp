8b7514b94bd451958ebdae32edd9232bf8410db3
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-21 15:16:36 -0300

    writing camera again

--- rpg/include/camera.hpp
+++ rpg/include/camera.hpp
@@ -9,19 +9,13 @@
 
 class camera {
 private:
-	const window& win;
-	vec2f cam_pos,
-		ref_xy1, ref_xy2,
-		lim_xy1, lim_xy2,
-		start_xy1, start_xy2;
+	vec2f cam_pos, ref;
 
 public:
-	camera(const window& win, const vec2f ref_xy1, const vec2f ref_xy2, const vec2f lim_xy1, const vec2f lim_xy2, const vec2f start_xy1 = vec2f(0, 0), const vec2f start_xy2 = vec2f(0, 0)) :
-	win(win), cam_pos(vec2f(0, 0)), ref_xy1(ref_xy1), ref_xy2(ref_xy2), lim_xy1(lim_xy1), lim_xy2(lim_xy2), start_xy1(start_xy1), start_xy2(start_xy2) { }
+	camera(const vec2f ref) :
+	cam_pos(0, 0), ref(ref) { }
 	~camera() { }
 
 	vec2f get_cam_pos() const { return this->cam_pos; }
-
-	void update_cam(const vec2f& new_xy1, const vec2f& new_xy2);
 
 };
