50ccf76da1dba5e2548fed0594a7c045fb4d2b14
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-02-21 15:33:10 -0300

    update_cam()

--- rpg/src/camera.cpp
+++ rpg/src/camera.cpp
@@ -3,5 +3,18 @@
 
 
 void camera::update_cam(const vec2f new_ref) {
-	
+	const vec2f ref_diff = vec2f(new_ref - ref);
+
+	if (
+		ref_diff.x > this->start.x ||
+		ref_diff.x < -(this->start.x)
+	)
+		this->cam_pos.x += ref_diff.x;
+
+	if (
+		ref_diff.y > this->start.y ||
+		ref_diff.y < -(this->start.y)
+	)
+		this->cam_pos.y += ref_diff.y;
+
 }
