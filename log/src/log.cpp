97eda8e317ffa5c57b5619aff5f347d059257441
Paul0TokiCrew <paul0eiry0@gmail.com>
2021-03-07 18:18:00 -0300

    back to pointers

--- log/src/log.cpp
+++ log/src/log.cpp
@@ -2,11 +2,13 @@
 
 
 
+std::vector<std::string*> log::status;
+
 void log::add_status(std::string* stat) {
 	log::status.push_back(stat);
 }
 
-void log::debug() {
+void log::log_status() {
 
 	for (auto i : log::status)
 		std::cout << *i << std::endl;
